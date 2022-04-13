f852e2d9115ea20f3a605c143de5a6e76c7daf40
Marissa Lazo <marissalazo@Marissas-MacBook-Pro.local>
2022-04-12 23:23:34 -0400

    adds controlled AddTodo component and destructures props

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,27 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoListItem from "./TodoListItem"
 
-const todoList = [
-    { id:1,
-      title: "complete assignment"
-    },
-    { id:2,
-      title: "walk dog"
-    },
-    { id:3,
-      title: "feed cat"
-    }
-  ];
-
-export default function TodoList() {
+export default function TodoList({todoList}) {
     return(
         <ul>
           {todoList.map((todo)=> {
-            return( <TodoListItem key={todo.id} todo={todo}/>
-           
-            );
+            return( <TodoListItem key={todo.id} todo={todo}/>);
           })}
         </ul>
-    
     );
 }
