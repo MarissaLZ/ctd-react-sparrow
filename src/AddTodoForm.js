748509959857b0f5b351249f0c4e7fe04c8a697c
Marissa Lazo <marissalazo@Marissas-MacBook-Pro.local>
2022-04-25 22:39:45 -0400

    adds custom hook

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,23 +1,24 @@
 import React from "react";
+import { useEffect } from "react/cjs/react.production.min";
 
 export default function AddTodoForm({onAddTodo}) {
+    console.log("addtodoForm component state")
     //state hook
     const [todoTitle, setTodoTitle] = React.useState("")
 
     //triggers every time something is typed into search bar
     const handleTitleChange = (event)=> {
         const newTodoTitle = event.target.value
-        console.log(newTodoTitle)
         setTodoTitle(newTodoTitle) //updates state every time something typed
     }
     //triggers when form submitted
-    //lifting state/event handler passes state info to share with state in app component
+    //event handler passes state info to share with state in app component
     const handleAddTodo = (event) => {
+        console.log("callback handleAddTodo")
         event.preventDefault()
-        onAddTodo({ title: todoTitle, id: Date.now() })
+        onAddTodo({id: Date.now(), title: todoTitle}) //passes object literal
         setTodoTitle("") //updates state when form submitted
     }  
- 
     return (
         <form onSubmit={handleAddTodo} > 
             <label htmlFor="todoTitle">Title</label>
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 
 export default function TodoListItem ({todo}) {
+    console.log("TodoListItem component")
     return (
         <li>{todo.title}</li>
         );
