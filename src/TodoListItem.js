9aebdd85f5a2cfc17af9bb5ea3f2e55123905c00
Marissa Lazo <marissalazo@Marissas-MacBook-Pro.local>
2022-05-10 21:48:34 -0400

    calls airtable api and processes data

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -3,6 +3,6 @@ import React from 'react'
 export default function TodoListItem ({todo, onRemoveTodo}) {
     
     return (
-        <li>{todo.title} <button type="button" onClick={ () => {onRemoveTodo(todo.id)}}>Remove</button></li>
+        <li>{todo.fields.Title} <button type="button" onClick={ () => {onRemoveTodo(todo.id)}}>Remove</button></li>
     );
 }
